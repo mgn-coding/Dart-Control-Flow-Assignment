1c5de7483f449e0295f7f80e736499c413922e57
mgn-coding <markgnjeru@gmail.com>
2024-04-29 17:41:46 +0300

    My first commit

---
+++ Assignment.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print("Enter your number:");
+  var num = int.parse(stdin.readLineSync()!);
+
+  if (num > 10) {
+    print("Your number is greater than 10");
+  } else if (num < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
